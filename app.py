2b60ce9181dcfccb8fc08524ae4ad7e706795bdb
sannjaay <22bd1a056l@gmail.com>
2024-07-12 21:59:13 +0530

    this is done after or calling the functions for one function

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask,render_template
+from flask import *
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def funct():
     return render_template('index.html')
+@app.route('/admin')
+def funct1():
+    return "this is admin"
+@app.route('/student')
+def funct2():
+    return 'this is student'
+@app.route('/staff')
+def funct3():
+    return 'this is staff'
 
+@app.route('/user/<name>')
+def user(name):
+    if(name == 'admin'):
+        return redirect(url_for('funct1'))
+    if(name == 'student'):
+        return redirect(url_for('funct2'))
+    if(name == 'staff'):
+        return redirect(url_for('funct3'))
 if __name__ == '__main__':
     app.run(debug=True)
